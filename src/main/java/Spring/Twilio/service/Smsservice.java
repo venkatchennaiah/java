f7972447c86ef0fabc29a8dc4ba329f01bd64ac0
vc21168 <vc21168@ojas-dd518.corp.ojas-it.com>
2022-05-30 17:59:03 +0530

    "success"

---
+++ src/main/java/Spring/Twilio/service/Smsservice.java
@@ -0,0 +1,31 @@
+package Spring.Twilio.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import Spring.Twilio.Smsrequest;
+
+@Service
+public class Smsservice {
+
+	private final Twilioproperties twilioproperties;
+	
+	@Autowired
+	public Smsservice(Twilioproperties twilioproperties)
+	{
+		this.twilioproperties=twilioproperties;
+	}
+	
+	//send message to number
+	public String sendsms(Smsrequest smsrequest)
+	{
+     
+		Message message=Message.creator(new PhoneNumber(smsrequest.getNumber()), new PhoneNumber(twilioproperties.getFromNumber()), smsrequest.getMessage()).create();
+        return message.getStatus().toString();
+        
+	
+	}
+}
